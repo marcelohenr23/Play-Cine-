8d00332ac20a6457a6083cfd5c7fbf7bfecbc55b
marcelohenr23 <marcelodossantostorres366@gmail.com>
2026-07-22 20:17:10 -0300

    Remove specific movie entries from movies array
    Removed several movie entries from the movies array.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const movies = [
-    { title: "Assalto ao Poder", genre: "Ação", year: "2016", image: "img/assaltoaopoder.jpg", videoUrl: "https://www.youtube.com/embed/N0W97a9Z3q0" },
-    { title: "Deadpool & Wolverine", genre: "Ação", year: "2024", image: "img/deadvoreine.jpg", videoUrl: "https://www.youtube.com/embed/73_1biulkYk" },
-    { title: "O Livro de Eli", genre: "Ação", year: "2010", image: "img/el.jpg", videoUrl: "https://www.youtube.com/embed/BQq2NZrQbV8" },
     { title: "Em Ritmo de Fuga", genre: "Ação", year: "2017", image: "img/em ritmo de fuga.jpg", videoUrl: "https://www.youtube.com/embed/d96cjJhvlMA" },
     { title: "Implacável", genre: "Ação", year: "2021", image: "img/implacavel.jpg", videoUrl: "https://www.youtube.com/embed/ZsJz2TJAPy4" },
     { title: "Sem Limite", genre: "Ação", year: "2011", image: "img/semlimite.jpg", videoUrl: "https://www.youtube.com/embed/jNZsxDUjBc8" },
-    { title: "Tomb Raider", genre: "Aventura", year: "2018", image: "img/tombraid.jpg", videoUrl: "https://www.youtube.com/embed/8ndjsKmb4c0" },
     { title: "Vingança", genre: "Ação", year: "2019", image: "img/vinganca.jpg", videoUrl: "https://www.youtube.com/embed/ZsJz2TJAPy4" }
 ];
 
