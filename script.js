e87e710d8f8f139689ecc0b4a7b4f55f6c660e4e
marcelohenr23 <marcelodossantostorres366@gmail.com>
2026-07-26 01:11:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ const movies = [
     { title: "Toy Story 4", genre: "Animação", year: "2019", image: "img/story4.jpg", videoUrl: "" },
     { title: "Todo Mundo em Pânico 2", genre: "Comédia", year: "2001", image: "img/tdmundo2.jpg", videoUrl: "" },
     { title: "Todo Mundo em Pânico 3", genre: "Comédia", year: "2003", image: "img/tdmundo3.jpg", videoUrl: "" }
+    { title: "Jack o Caçador de Gigantes", genre: "Aventura", year: "2013", image: "img/jackgigante.jpg", videoUrl: "" }
+
 ];
 
 const series = [
